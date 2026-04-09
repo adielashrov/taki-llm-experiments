90f201be4d813d78582f7c6c4fab0a9ad7795082
adielas <adiel.ashrov@gmail.com>
2026-04-09 11:14:28 +0300

    Start with developing the python-taki - draft trial #2

--- python_taki_api/__init__.py
+++ python_taki_api/__init__.py
@@ -1,9 +1,11 @@
 from .python_agent import PythonAgent
+from .reduced_taki_game import ReducedTakiGame
 from .taki_agent import TakiAgent
 from .taki_game import TakiGame
 
 __all__ = [
     "PythonAgent",
+    "ReducedTakiGame",
     "TakiAgent",
     "TakiGame",
 ]
